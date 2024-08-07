4f7f121d642d7e0763ef57187a894f6c65a71e32
hady444 <hady444@github.com>
2024-08-07 20:47:46 +0300

    pascal triangle with python

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,9 @@
 from math import factorial
+
+
 def pascal_triangle(n):
     li = []
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         comp = []
         for j in range(i):
             comp.append(factorial(i-1) // (factorial(j) * factorial(i-1-j)))
